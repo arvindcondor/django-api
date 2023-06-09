0986bd8aa1f8bf2b31730ab6d301e77f7f91ac98
Arvind <arvind.pandey@candorworks.in>
2023-06-09 17:35:16 +0530

    try add model -interface

--- api/views.py
+++ api/views.py
@@ -8,46 +8,36 @@ import json
 
 
 class MyApiView(APIView):
-#         def get(self, request, postId=None):
-#             url ='https://jsonplaceholder.typicode.com/comments'
-#             if postId is not None:
-#                 params = {'postId': postId}
-#             else:
-#                 params = {}
-#             params = {'postId': postId}
-#             response = requests.get(url, params=params)
-#
-#             if response.status_code == 200:
-#                 data = response.json()
-#                 # Process the data as needed
-#                 # Return a response or render a template with the data
-#             else:
-#                 # Handle the case when the request fails
-#                 data = []
-#
-#             return Response(data)
+        # def get(self, request, id=None):
+        #     url =f'https://jsonplaceholder.typicode.com/posts/{id}'
+        #
+        #     response = requests.get(url)
+        #
+        #     if response.status_code == 200:
+        #         data = response.json()
+        #         print(data)
+        #         # Process the data as needed
+        #         # Return a response or render a template with the data
+        #     else:
+        #         # Handle the case when the request fails
+        #         data = []
+        #
+        #     return Response(data)
 
+#----------------------------------------------------------------------------------------------------
 
-    def get(self, request, id=''):
-        try:
-            # url = 'https://jsonplaceholder.typicode.com/posts/1/comments'
-            url = 'https://jsonplaceholder.typicode.com/users'
-            # payload = {'id': id}
-            data = request.data
-            print(data)
-            print(json.dumps(data))
+        def get(self, request, id=None):
+            url ='https://jsonplaceholder.typicode.com/posts/'
 
-            response = requests.get(url, data=json.dumps(data))
-            print(response)
-            print(response.status_code)
+            response = requests.get(url)
 
             if response.status_code == 200:
-                print(data)
-                print("=============================")
-                print(response.json())
-
                 data = response.json()
-                print(data)
+                print(json.dumps(data, indent=4))
+                print("++++++++++++")
+                print(type(data))
+                for model in data:
+                    student = MyModel(userId =  model['userId'], title = data[1].['email'],  body = data[1].['body']
                 # Process the data as needed
                 # Return a response or render a template with the data
             else:
@@ -55,20 +45,90 @@ class MyApiView(APIView):
                 data = []
 
             return Response(data)
-        except Exception as exception:
-            print(exception)
 
-        # def put(self, request):
-        #     url = 'https://jsonplaceholder.typicode.com/posts'
-        #     data = request.data  # Retrieve the data from the request body
-        #     headers = {
-        #         'Content-type': 'application/json; charset=UTF-8'
-        #     }
-        #
-        #     response = requests.put(url, data=json.dumps(data), headers=headers)
-        #     json_response = response.json()
-        #
-        #     return Response(json_response)
+
+
+    # ===================================================
+
+
+    # def get(self, request, id=''):
+    #     try:
+    #         # url = 'https://jsonplaceholder.typicode.com/posts/1/comments'
+    #         url = 'https://jsonplaceholder.typicode.com/users'
+    #         # payload = {'id': id}
+    #         data = request.data
+    #         print(data)
+    #         print(json.dumps(data))
+    #
+    #         response = requests.get(url, data=json.dumps(data))
+    #         print(response)
+    #         print(response.status_code)
+    #
+    #         if response.status_code == 200:
+    #             print(data)
+    #             print("=============================")
+    #             print(response.json())
+    #
+    #             data = response.json()
+    #             print(data)
+    #             # Process the data as needed
+    #             # Return a response or render a template with the data
+    #         else:
+    #             # Handle the case when the request fails
+    #             data = []
+    #
+    #         return Response(data)
+    #     except Exception as exception:
+    #         print(exception)
+        # ========================================================================================
+
+
+
+        def put(self, request, id= None):
+            url = f'https://jsonplaceholder.typicode.com/posts/{id}'
+            data = request.data  # Retrieve the data from the request body
+            print(data)
+
+            response = requests.put(url, data)
+            data = response.json()
+            print(data)
+
+            return Response(data)
+
+        def post(self, request, id=None):
+            url = f'https://jsonplaceholder.typicode.com/posts/{id}'
+            data = request.data  # Retrieve the data from the request body
+            print(data)
+
+            response = requests.put(url, data)
+            data = response.json()
+            print(data)
+
+            return Response(data)
+
+        def patch(self, request, id = None):
+            url = f'https://jsonplaceholder.typicode.com/posts/{id}'
+            data = request.data
+            print(data)
+
+            response = requests.patch(url, data)
+
+            data = response.json()
+            print(data)
+            return Response(data)
+
+        def delete(self, request, id=None):
+            url = f'https://jsonplaceholder.typicode.com/posts/{id}'
+
+            data = request.data
+            params = {'id': id}
+            response = requests.delete(url, params= params)
+            if response.status_code == 200:
+                return Response({"success"})
+            else:
+                return Response({"Unsuccessful"})
+
+
 
 
 
